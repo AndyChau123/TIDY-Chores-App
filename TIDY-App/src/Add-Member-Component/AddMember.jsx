8b24801e3b929d50515c67f99ac5d1f3df20dd28
AndyChau123 <achau2004@csu.fullerton.edu>
2025-09-25 10:48:16 -0700

    Add member button (#5)

---
+++ TIDY-App/src/Add-Member-Component/AddMember.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import "./AddMember.css";
+
+function AddMember() {
+
+  return (
+    <div className="MemberName">
+      <div className="User2">
+        <h3 className="AddUser2">User #2</h3>
+        <button className="UserButton2">Add Member
+            <br /> +
+        </button>
+      </div>
+
+      <div className="User3">
+        <h3 className="AddUser3">User #3</h3>
+        <button className="UserButton3">Add Member
+            <br /> +
+        </button>
+      </div>
+
+      <div className="User4">
+        <h3 className="AddUser4">User #4</h3>
+        <button className="UserButton4">Add Member
+            <br /> +
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default AddMember;
