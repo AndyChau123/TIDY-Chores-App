e9fd2e33bf9e3e30b2c5e181b0378b29915e6a39
Kenneth <123523257+KenTree@users.noreply.github.com>
2025-09-29 17:46:47 -0700

    Added Chores List, expand / collapse button (#6)

--- TIDY-App/src/App.jsx
+++ TIDY-App/src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import './App.css'
 import AddMember from "./Add-Member-Component/AddMember.jsx"
+import ChoreList from './Chore-List-Component/ChoreList.jsx'
 
 function App() {
 
 
   return (
-    <AddMember/ >
+    <>
+      <AddMember />
+      <ChoreList />
+    </>
   )
 }
 
