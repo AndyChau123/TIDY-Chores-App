305ee21991c93de721f2a1ded8ba98e6981afdde
AndyChau123 <achau2004@csu.fullerton.edu>
2025-11-06 10:20:47 -0800

    created firebase database (#9)

---
+++ TIDY-App/src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA6i6u4gHX_MwintfmgL28oFoQjufLYBd4",
+  authDomain: "tidy-8a203.firebaseapp.com",
+  projectId: "tidy-8a203",
+  storageBucket: "tidy-8a203.firebasestorage.app",
+  messagingSenderId: "215404911667",
+  appId: "1:215404911667:web:c179139d1c7060e5f59746"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export {db};
